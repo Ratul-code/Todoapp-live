b698fec8d64ab21b67f57827a3eb2e55eee7e338
Ratul <rafiratul01@gmail.com>
2021-03-15 22:09:44 +0600

    django-heroku4

--- Todoapp/settings.py
+++ Todoapp/settings.py
@@ -138,6 +138,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
